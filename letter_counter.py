fbf9ce95448885efe42e63b6ad9a372d5f126083
vipullyadavv5-oss <vipullyadavv5@gmail.com>
2026-07-06 20:45:56 +0530

    Create letter_counter.py

---
+++ letter_counter.py
@@ -0,0 +1,13 @@
+text = input("Enter a string: ")
+
+upper = 0
+lower = 0
+
+for ch in text:
+    if ch.isupper():
+        upper += 1
+    elif ch.islower():
+        lower += 1
+
+print("Uppercase Letters:", upper)
+print("Lowercase Letters:", lower)
