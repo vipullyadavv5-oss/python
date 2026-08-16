86204b3ee92a6a35330673b84a0619ef9084099b
ILuvvanillaaicecream <vipullyadavv5@gmail.com>
2026-08-16 19:00:26 +0530

    Create 1. search_insert_position.py

---
+++ 1. search_insert_position.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
