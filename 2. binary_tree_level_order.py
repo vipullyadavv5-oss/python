c1116879cafbdf4ecf0480c24a6ced135185e6db
ILuvvanillaaicecream <vipullyadavv5@gmail.com>
2026-08-22 15:45:09 +0530

    Create 2. binary_tree_level_order.py

---
+++ 2. binary_tree_level_order.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def levelOrder(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = [root]
+
+        while queue:
+            level = []
+
+            for i in xrange(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+
+        return result
