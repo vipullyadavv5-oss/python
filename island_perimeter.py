6ec70172501687a7a2a798e5106961f4dbd55452
ILuvvanillaaicecream <vipullyadavv5@gmail.com>
2026-08-23 10:07:12 +0530

    Implement island perimeter calculation

---
+++ island_perimeter.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+
+        for i in xrange(rows):
+            for j in xrange(cols):
+                if grid[i][j] == 1:
+                    perimeter += 4
+
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
