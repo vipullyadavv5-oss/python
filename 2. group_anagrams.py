a0df7ef3af63a586d73f595af8ea9c24ebc0ae11
ILuvvanillaaicecream <vipullyadavv5@gmail.com>
2026-08-25 21:07:21 +0530

    Implement function to group anagrams

---
+++ 2. group_anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return groups.values()
