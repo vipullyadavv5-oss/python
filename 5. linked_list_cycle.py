d457ea2780a82d53754d5ebf35574ac2470feeff
ILuvvanillaaicecream <vipullyadavv5@gmail.com>
2026-08-26 08:08:56 +0530

    Implement cycle detection in linked list

---
+++ 5. linked_list_cycle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow is fast:
+                return True
+
+        return False
