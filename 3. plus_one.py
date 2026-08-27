354149182abef5ba2445d9dbc144d02369a915fd
ILuvvanillaaicecream <vipullyadavv5@gmail.com>
2026-08-27 23:37:21 +0530

    Implement plusOne method to increment digit array

---
+++ 3. plus_one.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in xrange(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        return [1] + digits
